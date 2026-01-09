641a15154f1dbb96557584342155fc7963c4734d
Juan Joaquin <juanmanueljoaquin@hotmail.com>
2026-01-08 21:51:24 -0300

    no fk

--- domain/enrollment.go
+++ domain/enrollment.go
@@ -10,13 +10,24 @@ import (
 type Enrollment struct {
 	ID        string     `json:"id" gorm:"type:char(36);not null;primary_key;unique_index"`
 	UserID    string     `json:"user_id,omitempty" gorm:"type:char(36)"`
+	User      *User      `json:"user,omitempty" gorm:"-"`
+	CourseID  string     `json:"course_id" gorm:"type:char(36);not null"`
+	Course    *Course    `json:"course,omitempty" gorm:"-"`
+	Status    string     `json:"status" gorm:"type:char(2)"`
+	CreatedAt *time.Time `json:"-"`
+	UpdatedAt *time.Time `json:"-"`
+}
+
+/* type Enrollment struct {
+	ID        string     `json:"id" gorm:"type:char(36);not null;primary_key;unique_index"`
+	UserID    string     `json:"user_id,omitempty" gorm:"type:char(36)"`
 	User      *User      `json:"user,omitempty"`
 	CourseID  string     `json:"course_id" gorm:"type:char(36);not null"`
 	Course    *Course    `json:"course,omitempty"`
 	Status    string     `json:"status" gorm:"type:char(2)"`
 	CreatedAt *time.Time `json:"-"`
 	UpdatedAt *time.Time `json:"-"`
-}
+} */
 
 func (c *Enrollment) BeforeCreate(tx *gorm.DB) (err error) {
 	if c.ID == "" {

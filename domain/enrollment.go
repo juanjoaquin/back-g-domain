3c159305b748db054aabd5d4249076a45674a1b8
Juan Joaquin <juanmanueljoaquin@hotmail.com>
2026-01-08 22:07:53 -0300

    no fg 2

--- domain/enrollment.go
+++ domain/enrollment.go
@@ -8,11 +8,11 @@ import (
 )
 
 type Enrollment struct {
-	ID        string     `json:"id" gorm:"type:char(36);not null;primary_key;unique_index"`
+	ID        string     `json:"id" gorm:"type:char(36);not null;primaryKey;uniqueIndex"`
 	UserID    string     `json:"user_id,omitempty" gorm:"type:char(36)"`
-	User      *User      `json:"user,omitempty" gorm:"-"`
-	CourseID  string     `json:"course_id" gorm:"type:char(36);not null"`
-	Course    *Course    `json:"course,omitempty" gorm:"-"`
+	User      *User      `json:"user,omitempty" gorm:"-"` //Aqui se agrega gorm:"-"`
+	CourseID  string     `json:"course_id,omitempty" gorm:"type:char(36)"`
+	Course    *Course    `json:"course,omitempty" gorm:"-"` //Aqui se agrega gorm:"-"`
 	Status    string     `json:"status" gorm:"type:char(2)"`
 	CreatedAt *time.Time `json:"-"`
 	UpdatedAt *time.Time `json:"-"`
